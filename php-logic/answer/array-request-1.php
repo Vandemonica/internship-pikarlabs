74a9e3cda6559f8aced36b00eaff2a1eebb901d2
Vandemonica <66208743+Vandemonica@users.noreply.github.com>
2021-01-06 11:17:59 +0700

    Solved permasalahan array-request-1

---
+++ php-logic/answer/array-request-1.php
@@ -0,0 +1,64 @@
+<?php
+$input = [
+    [ 'id' => 122, 'nama' => 'rama', 'asal' => 'surabaya' ],
+    [ 'id' => 227, 'nama' => 'renaldy', 'asal' => 'surabaya' ],
+    [ 'id' => 423, 'nama' => 'fakhri', 'asal' => 'surabaya' ],
+  ];
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        th{
+            border: 1px solid black;
+            padding: 4px;
+        }
+        td{
+            border: 1px solid black;
+            padding: 4px;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Request 1</title>
+</head>
+<body>
+    <?php if(isset($_GET["id"])):?>
+        <?php foreach($input as $inp => $val):?>
+            <?php if($val["id"] == $_GET["id"]):?>
+
+                <p> Profil siswa ID = <?=$val["id"];?> </p>
+
+                <ul>
+                    <li> ID : <?=$val["id"];?>       </li>
+                    <li> Nama : <?=$val["nama"];?>   </li>
+                    <li> Asal : <?=$val["asal"];?>   </li>
+                </ul>
+
+                <a href="array-request-1.php">Kembali</a>
+
+            <?php endif;?>
+        <?php endforeach;?>
+    <?php else:?>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Asal</th>
+                <th>Link</th>
+            </tr>
+            <?php foreach($input as $key => $v):?>
+            <tr>
+                <td> <?=$v["id"];?>   </td>
+                <td> <?=$v["nama"];?> </td>
+                <td> <?=$v["asal"];?> </td>
+                <td> <a href="?id=<?=$v['id'];?>"> profil </a> </td>
+            </tr>
+            <?php endforeach;?>
+        </table>
+    <?php endif;?>
+</body>
+</html>
