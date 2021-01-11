3e5d81b2976cb0612afb0c2cf91e53d53a013fb5
Ivan Nircahya <ivanxirko1987@gmail.com>
2021-01-12 00:02:00 +0700

    fix kesalahan kecil (lagi)

--- php-logic/answer/komponen/header.php
+++ php-logic/answer/komponen/header.php
@@ -12,6 +12,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../_include/tabela.css">
-    <title>Array Request 2</title>
+    <title>Array Request</title>
 </head>
 <body>
